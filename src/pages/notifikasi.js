d5e7e5bf48b2dbc1595a9725527424c5c5903817
telkomdev-satmika <mikaozora08@gmail.com>
2022-04-23 18:05:29 +0700

    add main page notification

--- src/pages/notifikasi.js
+++ src/pages/notifikasi.js
@@ -2,12 +2,15 @@ import React from "react";
 import { useHistory } from "react-router";
 import { Grid, Typography, Button, Box } from "@mui/material";
 import Navigation from "../components/navigation";
+import TopController from "../components/topController";
+import linkaja from "../assets/linkaja.png";
+import gopay from "../assets/gopay.png";
 
 const Notifikasi = () => {
   const history = useHistory();
 
   const handleHome = () => {
-      history.push("/")
+    history.push("/");
   };
   return (
     <div
@@ -23,7 +26,159 @@ const Notifikasi = () => {
           minHeight: "100vh",
         }}
       >
-        <Typography>Halooo ini notifikasi</Typography>
+        <TopController title="Notifikasi" action={() => handleHome()} />
+        <Box
+          sx={{
+            backgroundColor: "#ffffff",
+            padding: "16px",
+            display: "flex",
+            marginBottom: "8px",
+          }}
+        >
+          <img src={linkaja} style={{ width: "56px", marginRight: "16px" }} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "poppins",
+                fontSize: "14px",
+                fontWeight: 600,
+                color: "#000000",
+              }}
+            >
+              Top Up Link Aja
+            </Typography>
+            <Typography
+              sx={{
+                fontFamily: "poppins",
+                fontSize: "10px",
+                fontWeight: 400,
+                color: "#000000",
+              }}
+            >
+              Selamat kamu telah berhasil melakukan top up sebesar Rp125.000
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "#ffffff",
+            padding: "16px",
+            display: "flex",
+            marginBottom: "8px",
+          }}
+        >
+          <img src={gopay} style={{ width: "56px", marginRight: "16px" }} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "poppins",
+                fontSize: "14px",
+                fontWeight: 600,
+                color: "#000000",
+              }}
+            >
+              Top Up Gopay
+            </Typography>
+            <Typography
+              sx={{
+                fontFamily: "poppins",
+                fontSize: "10px",
+                fontWeight: 400,
+                color: "#000000",
+              }}
+            >
+              Selamat kamu telah berhasil melakukan top up sebesar Rp10.000
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "#ffffff",
+            padding: "16px",
+            display: "flex",
+            marginBottom: "8px",
+          }}
+        >
+          <img src={linkaja} style={{ width: "56px", marginRight: "16px" }} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "poppins",
+                fontSize: "14px",
+                fontWeight: 600,
+                color: "#000000",
+              }}
+            >
+              Top Up Link Aja
+            </Typography>
+            <Typography
+              sx={{
+                fontFamily: "poppins",
+                fontSize: "10px",
+                fontWeight: 400,
+                color: "#000000",
+              }}
+            >
+              Selamat kamu telah berhasil melakukan top up sebesar Rp50.000
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "#ffffff",
+            padding: "16px",
+            display: "flex",
+            marginBottom: "8px",
+          }}
+        >
+          <img src={gopay} style={{ width: "56px", marginRight: "16px" }} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "poppins",
+                fontSize: "14px",
+                fontWeight: 600,
+                color: "#000000",
+              }}
+            >
+              Top Up Link Aja
+            </Typography>
+            <Typography
+              sx={{
+                fontFamily: "poppins",
+                fontSize: "10px",
+                fontWeight: 400,
+                color: "#000000",
+              }}
+            >
+              Selamat kamu telah berhasil melakukan top up sebesar Rp250.000
+            </Typography>
+          </Box>
+        </Box>
         <Navigation />
       </Box>
     </div>
