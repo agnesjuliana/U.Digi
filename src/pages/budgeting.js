102df35aa89f1ce7ef1c58c7fce4a5e6f0b0def1
Belvatd <belva_talitha_28rpl@telkomschools.sch.id>
2022-04-23 17:39:52 +0700

    create add budget function

--- src/pages/budgeting.js
+++ src/pages/budgeting.js
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
-import { Box } from "@mui/material";
+import { Box, Fab } from "@mui/material";
 import TopController from "../components/topController";
 import BudgetCard from "../components/budgetCard";
+import DialogAddBudget from "../components/dialogAddBudget";
+import AddIcon from '@mui/icons-material/Add';
 
 const Budgeting = () => {
 
+  const history = useHistory();
+  const [open, setOpen] = useState(false)
+
   useEffect(() => {
-    // dataPlay();
     localStorage.setItem('data', JSON.stringify(data));
   }, []);
-
-  const history = useHistory();
 
   const handleHome = () => {
     history.push("/");
   };
 
-  const [data, setData] = useState([
+  const openDialog = () => {
+    setOpen(true)
+  }
+  const closeDialog = () => {
+    setOpen(false)
+  }
+
+  const [data] = useState([
     {
       id: 1,
       category: "Makanan",
@@ -35,22 +44,6 @@ const Budgeting = () => {
       sisa: 250000
     },
   ])
-
-  // const progressbar1 = document.querySelector(".progress1");
-  //   progressbar1.style.width = progress + `%`;
-  // };
-
-  // const progressbar2 = document.querySelector(".progress2");
-
-  // const changeProgress2 = (progress) => {
-  //   progressbar2.style.width = progress + `%`;
-  // };
-
-  // const dataPlay = () => {
-  //   setTimeout(() => changeProgress1(35), 100);
-  //   setTimeout(() => changeProgress2(50), 100);
-  // }
-
 
   let cardList = data.map((data, index) => {
     return (
@@ -70,13 +63,25 @@ const Budgeting = () => {
     );
   });
 
+  const addBudget = () => {
+    setOpen(false)
+    data.push({
+      id: 3,
+      category: "Listrik",
+      percentage: 0,
+      totalBudget: 500000,
+      pengeluaran: 0,
+      sisa: 500000
+    })
+    localStorage.setItem("data", JSON.stringify(this.data));
+  }
+
   return (
     <div
       style={{
         display: "flex",
         justifyContent: "center",
-      }}
-    >
+      }}>
       <Box
         sx={{
           width: "480px",
@@ -89,22 +94,21 @@ const Budgeting = () => {
           action={() => handleHome()}
         />
         <div style={{ padding: "24px 16px 0px 16px" }}>
-          {/* <BudgetCard
-            category="Makanan"
-            chart={
-              <div class="progress-container">
-                <div class="progress"></div>
-              </div>
-            }
-            percentage=" 22%"
-            sisaBudget="100000"
-            pengeluaran="200000"
-            totalBudget="400000"
-          /> */}
           {cardList}
         </div>
+        <Fab
+          sx={{ marginLeft: "16px"}}
+          variant="extended"
+          color="primary"
+          aria-label="add"
+          onClick={() => openDialog()}>
+          <AddIcon sx={{ mr: 1 }} />
+          Tambah kategori pengeluaran
+        </Fab>
       </Box>
+      <DialogAddBudget open={open} closeDialog={() => closeDialog()} addBudget={() => addBudget()} />
     </div>
+
   );
 };
 export default Budgeting;
